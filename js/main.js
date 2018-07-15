b22936f06269264baa833811534ac1e0310fecaf
Dan Dela Rosa <dandelarosadeveloper@gmail.com>
2018-07-15 19:35:44 -0400

    Refactor the game loop

--- js/main.js
+++ js/main.js
@@ -2,35 +2,15 @@ const GAME_WIDTH = 640;
 const GAME_HEIGHT = 480;
 const FPS = 30;
 
-var currentScene;
-var nextScene;
-
 window.addEventListener("load", function(event) {
-  // setupGraphics must be called first
-  setupGraphics(GAME_WIDTH, GAME_HEIGHT);
-  
   initGameState();
   // This should only be called when you actually enter a battle, but we can put this here for now
   initBattleState();
-  currentScene = new LoadingLoadingScene();
+  var firstScene = new LoadingLoadingScene();
+
+  // setupGraphics must be called before setting up input
+  setupGraphics(GAME_WIDTH, GAME_HEIGHT);
   setupKeyboard();
   setupMouse();
-  setInterval(eachFrame, 1000/FPS);
+  setupGameLoop(FPS, firstScene);
 });
-
-function eachFrame() {
-  updateGame();
-  drawGame();
-  if (nextScene) {
-    currentScene = nextScene;
-    nextScene = null;
-  }
-}
-
-function updateGame() {
-  currentScene.update();
-}
-
-function drawGame() {
-  currentScene.draw();
-}
